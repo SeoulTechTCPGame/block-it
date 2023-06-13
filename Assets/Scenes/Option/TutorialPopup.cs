b20c82ef3d4af0fa4aee48718c7f01990163c6a0
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-06-13 15:44:05 +0900

    feat: Tutorial Popup feature

--- Assets/Scenes/Option/TutorialPopup.cs
+++ Assets/Scenes/Option/TutorialPopup.cs
@@ -1,24 +1,64 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class Step
+{
+    public Sprite image;
+    public string explanation;
+}
 
 public class TutorialPopup : MonoBehaviour
 {
-    [SerializeField] GameObject tutorialPopup;
-    [SerializeField] Button openButton;
-    [SerializeField] Button closeButton;
+    [SerializeField] GameObject _tutorialPopup;
+    [SerializeField] Button _openBtn;
+    [SerializeField] Button _closeBtn;
+    [SerializeField] GameObject _scene;
+    [SerializeField] TMP_Text _explain;
+    [SerializeField] Image _image;
+    [SerializeField] Step[] _steps;
+    [SerializeField] Button _backBtn;
+    [SerializeField] Button _nextBtn;
+
+    private int _currentStep = 0;
 
     private void Start()
     {
-        openButton.onClick.AddListener(OpenTutorialPopup);
-        closeButton.onClick.AddListener(CloseTutorialPopup);
-    }
+        _openBtn.onClick.AddListener(OpenTutorialPopup);
+        _closeBtn.onClick.AddListener(CloseTutorialPopup);
+        _backBtn.onClick.AddListener(ShowPreviousStep);
+        _nextBtn.onClick.AddListener(ShowNextStep);
 
-    public void OpenTutorialPopup()
-    {
-        tutorialPopup.SetActive(true);
+        _tutorialPopup.SetActive(false);
     }
-    public void CloseTutorialPopup()
+    private void OpenTutorialPopup()
     {
-        tutorialPopup.SetActive(false);
+        _tutorialPopup.SetActive(true);
+        _scene.SetActive(false);
+        ShowStep(_currentStep);
+    }
+    private void CloseTutorialPopup()
+    {
+        _tutorialPopup.SetActive(false);
+        _scene.SetActive(true);
+    }
+    public void ShowPreviousStep()
+    {
+        ShowStep(_currentStep - 1);
+    }
+    private void ShowNextStep()
+    {
+        ShowStep(_currentStep + 1);
+    }
+    private void ShowStep(int stepIndex)
+    {
+        _currentStep = Mathf.Clamp(stepIndex, 0, _steps.Length - 1);
+
+        _image.sprite = _steps[_currentStep].image;
+        _explain.text = _steps[_currentStep].explanation;
+
+        _backBtn.interactable = _currentStep > 0;
+        _nextBtn.interactable = _currentStep < _steps.Length - 1;
     }
 }
