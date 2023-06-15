3d444a46487d61c50cc729b43af299a241f4acc5
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-06-15 16:21:23 +0900

    fix: Improved TutorialPopup.cs algorithm

--- Assets/Scenes/Option/TutorialPopup.cs
+++ Assets/Scenes/Option/TutorialPopup.cs
@@ -6,16 +6,19 @@ using TMPro;
 public class Step
 {
     public Sprite Image;
-    public string Explanation;
+    public string UpperExplanation;
+    public string LowerExplanation;
 }
 
 public class TutorialPopup : MonoBehaviour
 {
+    [Header("Tutorial")]
     [SerializeField] GameObject _tutorialPopup;
     [SerializeField] Button _openBtn;
     [SerializeField] Button _closeBtn;
-    [SerializeField] GameObject _scene;
-    [SerializeField] TMP_Text _explain;
+    [SerializeField] GameObject _optionPanel;
+    [SerializeField] TMP_Text _upperExplain;
+    [SerializeField] TMP_Text _lowerExplain;
     [SerializeField] Image _image;
     [SerializeField] Step[] _steps;
     [SerializeField] Button _backBtn;
@@ -36,13 +39,13 @@ public class TutorialPopup : MonoBehaviour
     private void OpenTutorialPopup()
     {
         _tutorialPopup.SetActive(true);
-        _scene.SetActive(false);
+        _optionPanel.SetActive(false);
         ShowStep(_currentStep);
     }
     private void CloseTutorialPopup()
     {
         _tutorialPopup.SetActive(false);
-        _scene.SetActive(true);
+        _optionPanel.SetActive(true);
     }
     private void ShowPreviousStep()
     {
@@ -57,7 +60,8 @@ public class TutorialPopup : MonoBehaviour
         _currentStep = Mathf.Clamp(stepIndex, 0, _steps.Length - 1);
 
         _image.sprite = _steps[_currentStep].Image;
-        _explain.text = _steps[_currentStep].Explanation;
+        _upperExplain.text = _steps[_currentStep].UpperExplanation;
+        _lowerExplain.text = _steps[_currentStep].LowerExplanation;
 
         _backBtn.interactable = _currentStep > 0;
         _nextBtn.interactable = _currentStep < _steps.Length - 1;
