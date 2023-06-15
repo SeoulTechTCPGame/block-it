b290bac87e235721b0fd5bf7c72f35278e023d30
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-06-15 15:53:31 +0900

    feat: apply sound icon assests

--- Assets/Scenes/Option/OptionalController.cs
+++ Assets/Scenes/Option/OptionalController.cs
@@ -9,6 +9,9 @@ public class OptionalController : MonoBehaviour
     [SerializeField] GameObject _languageButtons;
     [SerializeField] Image _background;
     [SerializeField] RectTransform _checkmark;
+    [SerializeField] Image _bgmImage;
+    [SerializeField] Image _soundEffectImage;
+    [SerializeField] Sprite[] sprites;
 
     private SoundManager _soundManager;
     private float _toggleMinX = -8.5f;
@@ -30,10 +33,12 @@ public class OptionalController : MonoBehaviour
         // BGM 볼륨 로드
         float bgmVolume = _soundManager.BgmVolume;
         _bGMSlider.value = bgmVolume;
+        UpdateBgmImage(bgmVolume);
 
         // 사운드 이펙트 볼륨 로드
         float soundEffectVolume = _soundManager.SoundEffectVolume;
         _soundEffectSlider.value = soundEffectVolume;
+        UpdateSoundEffectImage(soundEffectVolume);
 
         // 진동 설정 로드
         bool vibrationEnabled = _soundManager.IsVibrationEnabled;
@@ -44,12 +49,15 @@ public class OptionalController : MonoBehaviour
         // BGM 볼륨 값을 변경
         _soundManager.BgmVolume = value;
         Debug.Log("BGM Volume: " + value);
+        UpdateBgmImage(value);
     }
     public void OnSoundEffectVolumeChanged(float value)
     {
         // 사운드 이펙트 볼륨 값을 변경
         _soundManager.SoundEffectVolume = value;
         Debug.Log("Sound Effect Volume: " + value);
+        UpdateSoundEffectImage(value);
+
     }
     public void OnVibrationToggleChanged(bool value)
     {
@@ -64,5 +72,36 @@ public class OptionalController : MonoBehaviour
         // 위치에 따라 checkmark 좌우로 움직임
         float targetX = value ? _toggleMinX : _toggleMaxX;
         _checkmark.anchoredPosition = new Vector2(targetX, 0f);
+    }
+    private void UpdateBgmImage(float volume)
+    {
+        if (volume <= 0.3f)
+        {
+            _bgmImage.sprite = sprites[0];
+        }
+        else if (volume >= 0.7f)
+        {
+            _bgmImage.sprite = sprites[2];
+        }
+        else
+        {
+            _bgmImage.sprite = sprites[1];
+        }
+    }
+
+    private void UpdateSoundEffectImage(float volume)
+    {
+        if (volume <= 0.3f)
+        {
+            _soundEffectImage.sprite = sprites[0];
+        }
+        else if (volume >= 0.7f)
+        {
+            _soundEffectImage.sprite = sprites[2];
+        }
+        else
+        {
+            _soundEffectImage.sprite = sprites[1];
+        }
     }
 }
