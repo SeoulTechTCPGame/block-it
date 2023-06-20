2e595b24782fb35edf67fd02fc7504e0dfecb010
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-06-20 16:10:12 +0900

    chore: cleanup the scenes(Home,Option scene)

--- Assets/Scenes/Home/MoveScene.cs
+++ Assets/Scenes/Home/MoveScene.cs
@@ -1,33 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnOptionButtonClicked()
+    public void ToOption()
     {
         SceneManager.LoadScene("Option");
     }
 
-    public void OnProfileButtonClicked() 
+    public void ToProfile() 
     {
         SceneManager.LoadScene("Profile");
     }
 
-    public void OnExitButtonClicked()
+    public void ToHome()
     {
         SceneManager.LoadScene("Home");
     }
