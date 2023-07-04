04651b74bec9e28d41487db5ab3ec72b8a4b5b53
Hyojeong Jun <junnie082@gmail.com>
2023-07-04 20:50:30 +0900

    update: changed GetMoveableCoords() to jump over the opponent

--- Assets/Scenes/PlayScene/Script/BFS.cs
+++ Assets/Scenes/PlayScene/Script/BFS.cs
@@ -11,29 +11,33 @@ class BFS
         Queue<Vector2Int> que = new Queue<Vector2Int>();
         que.Enqueue(new Vector2Int(col, row));
 
+        GameLogic gameLogic = new GameLogic();
+        int[,] visited = new int[9, 9];
+
         //// NSEW
         while (que.Count != 0)
         {
             row = que.Peek().y;
             col = que.Peek().x;
+            visited[row,col] = 1;
             if (pawnNum == 1 && row == 9 || pawnNum == 2 && row == 1) return true;
             // NORTH
-            if (IsNorthValid(row, col))
+            if (visited[row+1,col] != 1 && !gameLogic.IsPlankInTheNorth(row, col))
             { 
                 que.Enqueue(new Vector2Int(col, row+1));
             }
             // SOUTH
-            if (IsSouthValid(row, col))
+            if (visited[row-1, col] != 1 && !gameLogic.IsPlankInTheSouth(row, col))
             {
                 que.Enqueue(new Vector2Int(col, row-1));
             }
             // EAST
-            if (IsEastValid(row, col))
+            if (visited[row, col+1] != 1 && !gameLogic.IsPlankInTheEast(row, col))
             {
                 que.Enqueue(new Vector2Int(col+1, row));
             }
             // WEST
-            if (IsWestValid(row, col))
+            if (visited[row, col-1] != 1 && !gameLogic.IsPlankInTheWest(row, col))
             {
                 que.Enqueue(new Vector2Int(col-1, row));
             }
@@ -43,56 +47,5 @@ class BFS
 
         return false;
     }
-
-    private bool IsNorthValid(int row, int col)
-    {
-        GameLogic gameLogic = new GameLogic();
-        foreach (Plank plank in gameLogic.planks)
-        {
-            if (gameLogic.IsNorthNotValid(plank, row, col))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool IsSouthValid(int row, int col)
-    {
-        GameLogic gameLogic = new GameLogic();
-        foreach (Plank plank in gameLogic.planks)
-        {
-            if (gameLogic.IsSouthNotValid(plank, row, col))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool IsEastValid(int row, int col)
-    {
-        GameLogic gameLogic = new GameLogic();
-        foreach (Plank plank in gameLogic.planks)
-        {
-            if (gameLogic.IsEastNotValid(plank, row, col))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool IsWestValid(int row, int col)
-    {
-        GameLogic gameLogic = new GameLogic();
-        foreach (Plank plank in gameLogic.planks)
-        {
-            if (gameLogic.IsWestNotValid(plank, row, col))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+ 
 }
