1feaaa2842fbf31b9953f47cd5351669d669efe8
KimDa99 <hyhy61451@gmail.com>
2023-07-23 19:34:21 +0900

    add: Win State

---
+++ Assets/Scenes/PlayScene/UI/WinState.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinState : MonoBehaviour
+{
+    private Text textComponent;
+
+    private void Start()
+    {
+        // Get the Text component attached to this GameObject
+        textComponent = GetComponent<Text>();
+
+        if (textComponent == null)
+        {
+            Debug.LogError("No Text component found on this GameObject!");
+            return;
+        }
+
+        textComponent.enabled = false;
+    }
+
+    public void DisplayWin(EPlayer ePlayer)
+    {
+        textComponent.enabled = true;
+
+        if (ePlayer == EPlayer.Player1)
+        {
+            // Change text content
+            textComponent.text = "Player 1 Wins!";
+            // Change text color
+            textComponent.color = new Color(0.96f, 0.57f, 0.15f);
+
+        }
+        else
+        {
+            // Change text content
+            textComponent.text = "Player 2 Wins!";
+            // Change text color
+            textComponent.color = new Color(0.45f, 0.76f, 0.96f);
+        }
+    }
+}
