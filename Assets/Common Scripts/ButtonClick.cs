b5a6fb034d54d9c3440dd079220985165c17af8f
DESKTOP-2FTOKJ4\jing <50050003+seong0929@users.noreply.github.com>
2023-07-26 22:17:19 +0900

    feat: add Button Click Sound Effect

--- Assets/Common Scripts/ButtonClick.cs
+++ Assets/Common Scripts/ButtonClick.cs
@@ -4,14 +4,8 @@ using UnityEngine.UI;
 public class ButtonClick : MonoBehaviour
 {
     [SerializeField] AudioClip mClip;
-    private Button mButton;
 
-    private void Start()
-    {
-        mButton = GetComponent<Button>();
-        mButton.onClick.AddListener(PlayButtonSound);
-    }
-    private void PlayButtonSound()
+    public void PlayButtonSound()
     {
         SoundManager.instance.PlaySoundEffect(mClip);
     }
